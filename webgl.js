895aa5d1df4259b08d131a834196e76acc242d3f
CallumHoward <callumh@cse.unsw.edu.au>
2020-04-20 22:46:27 +1000

    stripped back for orthographic

--- webgl.js
+++ webgl.js
@@ -25,12 +25,7 @@ const sketch = ({ context }) => {
   renderer.setClearColor("#000", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(0, 2, -4);
-  camera.lookAt(new THREE.Vector3());
-
-  // Setup camera controller
-  const controls = new THREE.OrbitControls(camera, context.canvas);
+  const camera = new THREE.OrthographicCamera(50, 1, 0.01, 100);
 
   // Setup your scene
   const scene = new THREE.Scene();
@@ -40,9 +35,8 @@ const sketch = ({ context }) => {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
 
   // Setup a material
-  const material = new THREE.MeshPhysicalMaterial({
-    color: "white",
-    roughness: 0.75,
+  const material = new THREE.MeshBasicMaterial({
+    color: "red",
     flatShading: true,
   });
 
@@ -62,18 +56,37 @@ const sketch = ({ context }) => {
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight, false);
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 1.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world centre
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
       camera.aspect = viewportWidth / viewportHeight;
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
     render({ time }) {
       mesh.rotation.y = time * 0.1;
-      controls.update();
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
     unload() {
-      controls.dispose();
       renderer.dispose();
     },
   };
