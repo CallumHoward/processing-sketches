a109efc97006d3f1fec38dae838956ed30c03d19
CallumHoward <callumh@cse.unsw.edu.au>
2020-04-20 21:17:34 +1000

    using text and rotation

--- sketch.js
+++ sketch.js
@@ -19,9 +19,10 @@ const sketch = () => {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
         points.push({
-          radius: Math.abs(0.01 + random.noise2D(u, v) * 0.05),
+          radius: Math.abs(0.01 + random.noise2D(u, v) * 0.1),
           position: [u, v],
           color: random.pick(palette),
+          rotation: Math.abs(0.01 + random.noise2D(u, v)),
         });
       }
     }
@@ -35,15 +36,23 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    points.forEach(({ position, radius, color }) => {
+    points.forEach(({ position, radius, color, rotation }) => {
       const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
       context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(rotation);
+
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Arial"`;
+      context.fillText("=", 0, 0);
+
+      context.restore();
     });
   };
 };
