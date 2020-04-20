9ac09590747b8ff99a85c044ebcb9f5ea3e95296
CallumHoward <callumh@cse.unsw.edu.au>
2020-04-20 20:31:26 +1000

    random filtering

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 2048, 2048 ]
@@ -8,7 +9,7 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 20;
     for (x = 0; x < count; x++) {
       for (y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
@@ -19,7 +20,8 @@ const sketch = () => {
     return points;
   }
 
-  const points = createGrid();
+  random.setSeed(1);
+  const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 100;
 
   return ({ context, width, height }) => {
@@ -30,8 +32,9 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
       context.beginPath();
-      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.arc(x, y, 10, 0, Math.PI * 2, false);
       context.strokeStyle = 'black';
+      context.lineWidth = 5;
       context.stroke();
     });
   };
