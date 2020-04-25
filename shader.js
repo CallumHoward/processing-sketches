ca952472559086a34c6fa4c671503a151f3b610d
CallumHoward <callumh@cse.unsw.edu.au>
2020-04-25 15:20:06 +1000

    mix between two colours

--- shader.js
+++ shader.js
@@ -16,9 +16,12 @@ const frag = glsl(/* glsl */ `
   varying vec2 vUv;
 
   void main () {
-    vec3 colorA = vec3(sin(time) + 1.0);
-    vec3 colorB = vec3(vUv.x);
-    gl_FragColor = vec4(colorA, 1.0);
+    vec3 colorA = vec3(1.0, 0.0, 0.0);
+    vec3 colorB = vec3(0.0, 1.0, 0.0);
+
+    // Mix from A to B based on uv coords
+    vec3 color = mix(colorA, colorB, vUv.x);
+    gl_FragColor = vec4(color, 1.0);
   }
 `);
 
