da05a879c433ad6e5ffc66a04d5d7dd2e0fd44b1
CallumHoward <callumh@cse.unsw.edu.au>
2020-04-25 15:31:02 +1000

    aspect corrected circle by passing info in with uniform, white bg

--- shader.js
+++ shader.js
@@ -13,15 +13,21 @@ const frag = glsl(/* glsl */ `
   precision highp float;
 
   uniform float time;
+  uniform float aspect;  // Correct based on aspect ratio
   varying vec2 vUv;
 
   void main () {
     vec3 colorA = vec3(1.0, 0.0, 0.0);
     vec3 colorB = vec3(0.0, 1.0, 0.0);
 
+    // Change based on how far from the centre
+    vec2 center = vUv - 0.5; // same as vec2(0.5, 0.5);
+    center.x *= aspect;  // correct aspect ratio based on sketch size
+    float dist = length(center);
+
     // Mix from A to B based on uv coords
     vec3 color = mix(colorA, colorB, vUv.x);
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(color, dist > 0.25 ? 0.0 : 1.0);
   }
 `);
 
@@ -29,6 +35,8 @@ const frag = glsl(/* glsl */ `
 const sketch = ({ gl }) => {
   // Create the shader and return it
   return createShader({
+    // Backgrond color
+    clearColor: "white",
     // Pass along WebGL context
     gl,
     // Specify fragment and/or vertex shader strings
@@ -37,6 +45,7 @@ const sketch = ({ gl }) => {
     uniforms: {
       // Expose props from canvas-sketch
       time: ({ time }) => time,
+      aspect: ({ width, height }) => width / height,
     },
   });
 };
